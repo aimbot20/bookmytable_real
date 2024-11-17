a255cf687179087a452167c0021b56d00d361d26
aimbot20 <l226613@lhr.nu.edu.pk>
2024-11-17 18:24:34 +0500

    added login of customer only owner nothandled.

--- BookMyTable/BookMyTable/views.py
+++ BookMyTable/BookMyTable/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from users.models import Users
 
 def home(request):
-    user_id = request.GET.get('user_id')  # Capture user_id from query parameters
-    return render(request, 'website/index.html', {'user_id': user_id})
+    user_id = request.GET.get('user_id')  # Capture user ID from query parameters
+    user = None
 
+    if request.user.is_authenticated:  # Check if the user is logged in
+        user = request.user  # Use the logged-in user
+
+    # If user_id is provided in the URL, get the user object for that ID
+    if user_id and not user:
+        try:
+            user = Users.objects.get(id=user_id)
+        except Users.DoesNotExist:
+            user = None
+
+    return render(request, 'website/index.html', {'user': user})  # Pass the user to the template
 def contact(request):
     user_id = request.GET.get('user_id')  # Capture user_id from query parameters
     return render(request, 'website/contact.html', {'user_id': user_id})
